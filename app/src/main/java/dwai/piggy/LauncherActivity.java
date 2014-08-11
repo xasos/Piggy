4ec034f4b60e06be918c082cdafb04e732f655bc
Stefan <soccerfanatic1996@gmail.com>
2014-08-10 19:34:18 -0500

    added profile and the friends choser

--- app/src/main/java/dwai/piggy/LauncherActivity.java
+++ app/src/main/java/dwai/piggy/LauncherActivity.java
@@ -1,9 +1,16 @@
 package dwai.piggy;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
 
 
 public class LauncherActivity extends Activity {
@@ -12,25 +19,52 @@ public class LauncherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+
+        final Typeface mFont = Typeface.createFromAsset(getAssets(),
+                "fonts/proxima.ttf");
+        final ViewGroup mContainer = (ViewGroup) findViewById(
+                android.R.id.content).getRootView();
+        LauncherActivity.setAppFont(mContainer, mFont, true);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.launcher, menu);
-        return true;
+    public void pressedSignIn(View v){
+        startActivity(new Intent(LauncherActivity.this, Profile.class));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public static final void setAppFont(ViewGroup mContainer, Typeface mFont, boolean reflect)
+    {
+        if (mContainer == null || mFont == null) return;
+
+        final int mCount = mContainer.getChildCount();
+
+        // Loop through all of the children.
+        for (int i = 0; i < mCount; ++i)
+        {
+            final View mChild = mContainer.getChildAt(i);
+            if (mChild instanceof TextView)
+            {
+                // Set the font if it is a TextView.
+                ((TextView) mChild).setTypeface(mFont);
+            }
+            else if (mChild instanceof ViewGroup)
+            {
+                // Recursively attempt another ViewGroup.
+                setAppFont((ViewGroup) mChild, mFont,true);
+            }
+            else if (reflect)
+            {
+                try {
+                    Method mSetTypeface = mChild.getClass().getMethod("setTypeface", Typeface.class);
+                    mSetTypeface.invoke(mChild, mFont);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        return super.onOptionsItemSelected(item);
     }
+
+
+
 }
